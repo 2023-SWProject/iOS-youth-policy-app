5cc0e807cc3ac8aad27c48e4c7e8e8d787812d0b
김태성 <ts4683@gmail.com>
2023-04-01 00:45:41 +0900

    init

---
+++ YouthPolicyPedia/YouthPolicyPedia/YouthPolicyPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouthPolicyPediaApp.swift
+//  YouthPolicyPedia
+//
+//  Created by 김태성 on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct YouthPolicyPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
