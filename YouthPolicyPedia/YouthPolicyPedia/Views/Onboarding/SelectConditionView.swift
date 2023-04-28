289284338f91f6828ced4f1e70c639198b6e760f
KTS224 <ts4683@gmail.com>
2023-04-29 01:37:30 +0900

    [Chore] 뷰별 디렉토리 구조 변경

---
+++ YouthPolicyPedia/YouthPolicyPedia/Views/Onboarding/SelectConditionView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectConditionView.swift
+//  YouthPolicyPedia
+//
+//  Created by 김태성 on 2023/04/29.
+//
+
+import SwiftUI
+
+struct SelectConditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectConditionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectConditionView()
+    }
+}
